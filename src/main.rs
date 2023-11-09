ca2e75f5f5cb8c9db6806c129896a6f54c86d94c
KatCote <filtermr@gmail.com>
2023-11-09 05:03:16 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use castlecore::core::*;
+use castlecore::screen::*;
+use castlecore::window::*;
+use castlecore::render::*;
+
+fn main() { 
+
+    initpath();
+    initscr("Altenstein | Engine ", true);
+
+    write_default_game_window(
+        Screen::RenderLayer(RenderInterface::MapLayer(MapLayer::Base)),
+        Screen::RenderLayer(RenderInterface::MapLayer(MapLayer::Color)),
+        Screen::RenderLayer(RenderInterface::MapLayer(MapLayer::Trigger)),
+        Screen::RenderLayer(RenderInterface::MapLayer(MapLayer::Wall)),
+        Screen::RenderLayer(RenderInterface::MapLayer(MapLayer::Explore)),
+        0.8,
+        0.8,
+        0.5,
+        0.5);
+
+    usescr();
+    endscr();
 }
